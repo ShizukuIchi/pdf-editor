2cf85774298d363bda063431ea534fea6f441b58
sh1zuku <shizukuichi@gmail.com>
2020-11-04 00:41:12 +0800

    Fix rotation (#14)
    fix pdfs with origin rotation other than 0

--- src/PDFPage.svelte
+++ src/PDFPage.svelte
@@ -15,7 +15,7 @@
   async function render() {
     const _page = await page;
     const context = canvas.getContext("2d");
-    const viewport = _page.getViewport({ scale: 1 });
+    const viewport = _page.getViewport({ scale: 1, rotation: 0 });
     width = viewport.width;
     height = viewport.height;
     await _page.render({
